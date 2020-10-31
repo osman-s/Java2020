5b59bcaeb36c8630c3c3f8bd4cde3a1c58cc6fcd
osman-s <osmanshasan@gmail.com>
2020-10-31 00:53:37 -0400

    Primities data types

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,3 +1,16 @@
 public class Hello {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Hi Osman");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+
+        byte b1 = 3;
+        short s1 = 10000;
+        long l1 = 1000;
+
+
+
+    }
 }
