e4fa1254b16bfc104844f029ba0b5e9b3f16cbc6
osman-s <osmanshasan@gmail.com>
2020-11-01 14:43:20 -0500

    Logical Expressions

---
+++ Mains/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char myChar = 'D';
+        char myUnicode = '\u0044';
+        char myUnicode2 = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+        System.out.println(myUnicode2);
+        boolean trues = true;
+
+        String myString = "This is a string";
+        System.out.println(myString + ", Why?");
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("Not an alien");
+        }
+
+    }
+}
