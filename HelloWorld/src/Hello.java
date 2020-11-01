95c3c904b8a6ff196cf89813d97b31305aba1b6d
osman-s <osmanshasan@gmail.com>
2020-11-01 00:48:13 -0400

    floats and doubles

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -10,7 +10,14 @@ public class Hello {
         short s1 = 10000;
         long l1 = 1000;
 //        Small changes
+        int myTotal = (myFirstNumber / 2);
+        byte myNewByte = (byte) (myTotal / 2);
+        long myLong = 5000L + 10L * (myNewByte + myTotal);
+        System.out.println(myLong);
+        int first = s1;
+        System.out.println(first);
 
-
+        float num1 = 5.25f;
+        double num2 = 5.25d;
     }
 }
