36cab533515c603d8e098217a3d1495aebe126e4
osman-s <osmanshasan@gmail.com>
2020-11-02 04:45:49 -0500

    coding exercise 2

--- Mains/src/com/company/MegaBytesConverter.java
+++ Mains/src/com/company/MegaBytesConverter.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        int megabytes = (int) Math.floor(kiloBytes / 1024);
+        int remaining = kiloBytes % 1024;
+        System.out.println(kiloBytes + " KB = " + megabytes +
+                " MB and " + remaining + " KB");
+    }
 }
