48e38a1d04bee5353490defc3dc2f170d96e2fde
Osman S <osmanshasan@gmail.com>
2020-11-04 02:01:20 -0500

    SumOddRange

--- Switch/src/com/company/Main.java
+++ Switch/src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        System.out.println(SumOddRange.isOdd(24));
+        System.out.println(SumOddRange.sumOdd(100,1000));
     }
 }
---
+++ Switch/src/com/company/SumOddRange.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class SumOddRange {
+    public static boolean isOdd(int number) {
+        if (number < 0) {
+            return false;
+        }
+        if (number % 2 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static int sumOdd(int start, int end) {
+
+
+        if ((end >= start) && start > 0 && end > 0) {
+            int sum = 0;
+            for (int i=start; i <= end; i++) {
+                if(isOdd(i)) {
+                    sum += i;
+                }
+            }
+            return sum;
+        } else {
+            return -1;
+        }
+    }
+}
