00794a91adfebb4a4832a4d260e4eba21fd708ce
Osman S <osmanshasan@gmail.com>
2020-11-04 03:50:49 -0500

    small changes

--- Switch/src/com/company/Main.java
+++ Switch/src/com/company/Main.java
@@ -6,5 +6,9 @@ public class Main {
 	// write your code here
         System.out.println(SumOddRange.isOdd(24));
         System.out.println(SumOddRange.sumOdd(100,1000));
+
+        for(int i=1; i < 7; i++) {
+            System.out.println(i);
+        }
     }
 }
